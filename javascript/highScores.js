fc755b8824f20898d14e319075b54193bd33ba79
Abby Kuranz <akuranz@gmail.com>
2019-11-15 15:24:15 -0700

    stores initials and scores in local storage, prints to high scores

--- javascript/highScores.js
+++ javascript/highScores.js
@@ -1,23 +1,36 @@
 //HighScores Page
 var clearHistory = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
-
-clearHistory.addEventListener("click", localStorage.clear());
-
-goBack.addEventListener("click", function(event) {
-    window.location.href = "startQuiz.html";
-  })
-
-var highScoreRecord = localStorage.getItem("highScores");
-var highScoreRecordPrint = JSON.parse(highScoreRecord);
-
 var highScoresList = document.querySelector("#high-scores-list");
+var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
-window.addEventListener('load', renderHighScores);
+clearHistory.addEventListener("click", function() {
+  localStorage.removeItem("highscores");
+  window.location.reload();
+});
 
-function renderHighScores () {
-  var li = document.createElement("li");
-  li.innerHTML = highScoreRecordPrint;
-  highScoresList.append(li); 
+window.addEventListener("load", renderHighScores);
+goBack.addEventListener("click", function(event) {
+  window.location.href = "index.html";
+});
+
+function renderHighScores() {
+  var sorted = highscores.sort(function(a, b) {
+    if (a.score < b.score) {
+      return 1;
+    }
+    return -1;
+  });
+
+  for (var i = 0; i < sorted.length; i++) {
+    console.log(highscores[i].score);
+    var li = document.createElement("li");
+    li.innerHTML =
+      "<h2> INITIALS: " +
+      highscores[i].initials +
+      " | SCORE: " +
+      highscores[i].score +
+      "</h2>";
+    highScoresList.append(li);
+  }
 }
-    
--- javascript/startQuiz.js
+++ javascript/startQuiz.js
@@ -6,173 +6,155 @@ var finalScore = document.querySelector("#final-score");
 var finalAnswer = document.querySelector("#answers");
 var enterInitials = document.querySelector("#enter-initials");
 var submitInitials = document.querySelector("#submit-initials");
-
-var questions = [
-  {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-  },
-  {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "curly brackets"
-  },
-  {
-      title: "Arrays in javascript can be used to store ____.",
-      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-      answer: "all of the above"
-  },
-  {
-      title: "String values must be enclosed with ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
-  },
-  {
-      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["javascript", "terminal/bash", "for loops", "console log"],
-      answer: "console log"
-  },
-  
-];
-
-
-window.addEventListener('load', renderQuestion);
 var answers = document.querySelector("#answers");
 var ansEval = document.querySelector("#evaluate-answer");
 var timer = document.querySelector("#timer");
+
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "curly brackets"
+  },
+  {
+    title: "Arrays in javascript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "String values must be enclosed with ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["javascript", "terminal/bash", "for loops", "console log"],
+    answer: "console log"
+  }
+];
 let currentQuestionIndex = 0;
 let q = questions[currentQuestionIndex];
 let score = 0;
 var timeLeft = 75;
+var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
-//Time interval starts at 75, decreases by 15 if question answered incorrectly, 
+//EVENTS
+
+window.addEventListener("load", renderQuestion);
+
+startBtn.addEventListener("click", Start);
+
+answers.addEventListener("click", checkAnswer);
+
+finalAnswer.addEventListener("click", finalScorePage);
+
+submitInitials.addEventListener("click", function(event) {
+  event.preventDefault();
+  var initials = enterInitials.value;
+  var newScores = {
+    initials,
+    score
+  }
+  highscores.push(newScores);
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+
+  console.log(initials, score);
+
+  window.location.href = "HighScores.html";
+});
+
+
+//FUNCTIONS
+
+function Start(){
+  currentQuestionIndex = 0;
+  console.log(highscores)
+
+  startContainer.setAttribute("style", "display: none");
+  questionContainer.setAttribute("style", "display: block");
+  startTimer();
+}
+//Time interval starts at 75, decreases by 15 if question answered incorrectly,
 //clears at 0 and when last question answered
 function startTimer() {
   var timeInterval = setInterval(function() {
-  timeLeft--;
-  localStorage.setItem("timer", timeLeft);
-  timer.textContent ="Time: " + timeLeft;
-  if (timeLeft === 0) {
-    timer.textContent = "Time's up!";
-    clearInterval(timeInterval);
-  }
-  if (currentQuestionIndex === 5) {
-    timer.textContent = "Great work!";
-    clearInterval(timeInterval);
-  }
-
-}, 1000);  
+    timeLeft--;
+    localStorage.setItem("timer", timeLeft);
+    timer.textContent = "Time: " + timeLeft;
+    if (timeLeft === 0) {
+      timer.textContent = "Time's up!";
+      clearInterval(timeInterval);
+    }
+    if (currentQuestionIndex === 5) {
+      timer.textContent = "Great work!";
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 }
 
-startBtn.addEventListener("click", startTimer);
-
-function nextContainer(event) {
-  startContainer.setAttribute("style", "display: none");
-  questionContainer.setAttribute("style", "display: block");
-}
-startBtn.addEventListener("click", nextContainer)
 
 //Render the first question from array
-function renderQuestion(){
+function renderQuestion() {
   document.querySelector("#question-title").innerHTML = q.title;
   document.getElementById("0").innerHTML = "1 " + q.choices[0];
   document.getElementById("1").innerHTML = "2 " + q.choices[1];
   document.getElementById("2").innerHTML = "3 " + q.choices[2];
-  document.getElementById("3").innerHTML = "4 " + q.choices[3];    
+  document.getElementById("3").innerHTML = "4 " + q.choices[3];
 }
 
 //Check the index of the choice linked to button against answer in array
 function checkAnswer(event) {
   q = questions[currentQuestionIndex];
   event.preventDefault();
-  if(event.target.matches("button")) {
-      var userAnswer = questions[currentQuestionIndex].choices[event.target.id];
-      console.log(userAnswer);
-      var correctAnswer = questions[currentQuestionIndex].answer;
-      console.log(correctAnswer);
+  if (event.target.matches("button")) {
+    var userAnswer = questions[currentQuestionIndex].choices[event.target.id];
+    console.log(userAnswer);
+    var correctAnswer = questions[currentQuestionIndex].answer;
+    console.log(correctAnswer);
   }
   if (userAnswer === correctAnswer) {
     score += 10;
     console.log(score);
-  }  else 
-    timeLeft -= 15;
+  } else timeLeft -= 15;
   if (userAnswer === correctAnswer) {
-          ansEval.textContent = "Correct Answer!"
-    
-      } else {
-          ansEval.textContent = "Wrong Answer!"
-      }
+    ansEval.textContent = "Correct Answer!";
+  } else {
+    ansEval.textContent = "Wrong Answer!";
   }
-answers.addEventListener("click", checkAnswer);
+  nextQuestion();
+}
 
 //has to be after check answer
-function nextQuestion (event) {
+function nextQuestion() {
   console.log("incrementing");
-  if (currentQuestionIndex < questions.length ) {
-      currentQuestionIndex++;
-      q = questions[currentQuestionIndex];
-      renderQuestion();
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    q = questions[currentQuestionIndex];
+    renderQuestion();
   }
-  
+
   console.log(currentQuestionIndex);
 }
-answers.addEventListener("click", nextQuestion);
 
-//displays final score page wih final score 
+//displays final score page wih final score
 function finalScorePage(event) {
   q = questions[currentQuestionIndex];
-  if(event.target.matches("button") && currentQuestionIndex === 5) {
+  if (event.target.matches("button") && currentQuestionIndex === 5) {
     questionContainer.setAttribute("style", "display: none");
     scoreContainer.setAttribute("style", "display: block");
     finalScore.textContent = "Your final score is " + (score += timeLeft);
+  }
+  console.log("finalscorepage");
 }
-console.log("finalscorepage");
-}
-finalAnswer.addEventListener("click", finalScorePage);
-
-//None of this seems to work correctly
-var initials = enterInitials.value;
-
-
-var highScores = [{
-    initials: initials,
-    score: score
-}];
-
-function pushToHighscoreArr(event) {
-  event.preventDefault();
-  var initials = enterInitials.value;
-  highScores.push(initials);
-  highScores.push(score);
-  console.log("highscore array", highScores);
-}
-
-submitInitials.addEventListener("click", pushToHighscoreArr);
-
-function logInitials () {
-  var highscoresStringified = JSON.stringify(highScores);
-  localStorage.setItem("Highscores", highscoresStringified);
-}
-submitInitials.addEventListener("click", logInitials);
-
-submitInitials.addEventListener("click", function() {
-  window.location.href = "HighScores.html";
-})
-
-
-// //HighScores Page
-// var clearHistory = document.querySelector("#clear");
-// var goBack = document.querySelector("#goBack");
-// var loggedHighScores = localStorage.getItem("Initials");
-// var highScores = document.querySelector("#high-scores");
-
-// window.addEventListener('load', function () {
-//   highScores.textContent = loggedHighScores;
-// });
-
-// clearHistory.addEventListener("click", localStorage.clear);
-
-// goBack.addEventListener("click", function(event) {
-//     window.location.href = "startQuiz.html";
-//   })
